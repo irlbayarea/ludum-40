f86c87b27a310fd576f0706752ab0fafca47b2c6
Matan Lurey <matanlurey@users.noreply.github.com>
2017-11-26 11:45:46 -0800

    Fix the build. (#2)

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,6 @@
 const path = require('path');
 const webpack = require('webpack');
 
-const CleanWebpackPlugin = require('clean-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 const nodeEnv = process.env.NODE_ENV || 'development';
