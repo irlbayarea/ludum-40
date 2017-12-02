e8b7164b44704002f261ec98b75eab2880118430
QuantumBits <classicrockjunkie@gmail.com>
2017-12-02 00:25:16 -0800

    Changed events.ts to crisis.ts, added more logical crisis management, added Guard type to character.ts

--- src/character.ts
+++ src/character.ts
@@ -6,24 +6,27 @@ class Character {
 
     name: string;
 
-    str: number;
-    int: number;
-    cha: number;
-
+    speed: number;
+    
     x: number;
     y: number;
     
-    constructor(name: string, str: number, int: number, cha: number, x: number, y: number) {
-
+    constructor(name: string, x: number, y: number, speed: number) {
+        
         this.name = name;
-        
-        this.setStr(str);
-        this.setInt(int);
-        this.setCha(cha);
-        
+        this.setSpeed(speed);
         this.moveTo(x, y);
     }
     
+    setSpeed(speed: number) {
+        if (speed > 0 ) {
+        this.speed = speed;
+        }
+        else {
+            throw new RangeError("Speed must be > 0");
+        }
+    }
+
     moveBy(dx: number, dy: number): void {
         this.x = this.x + dx;
         this.y = this.y + dy;
@@ -33,6 +36,24 @@ class Character {
         if (x > 0) { this.x = x; } else { throw new RangeError("x value must be > 0"); }
         if (y > 0) { this.y = y; } else { throw new RangeError("y value must be > 0"); }
     }
+    
+}
+
+
+class Guard extends Character {
+    
+    str: number;
+    int: number;
+    cha: number;
+    
+    constructor(name: string, x: number, y: number, speed: number,
+                str: number, int: number, cha: number) {
+        super(name, x, y, speed);
+        this.setStr(str);
+        this.setInt(int);
+        this.setCha(cha);
+    }
+    
     
     setStr(str: number): void {
         if (str > 0) { this.str = str; } else { throw new RangeError("str value must be > 0"); }
@@ -51,7 +72,7 @@ class Character {
 function generateName(): string {
     
     let names: Array<string> = ["Bob", "Joe", "Bill", "Steve", "Eric", "Donald", "Jared", "Robert", "Paul"];
-
+    
     return names[Math.floor(Math.random()*names.length)];
-
+    
 }
