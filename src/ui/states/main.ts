fa3d848e3fd7f1d7d492cde3b4c85bacb0e3260a
Alexander J Kindel <alexkindel@gmail.com>
2017-12-02 04:08:34 -0800

    Timer stuff (#26)
    * RepeatingTimer
    
    * Time and Timers. USE TIME FOR ALL TIME NEEDS.
    
    * Unbreaking main. No monster movement for now.
    
    * Timer millisecond -> second bug fix.

--- src/ui/states/main.ts
+++ src/ui/states/main.ts
@@ -36,7 +36,7 @@ export default class Main extends Phaser.State {
     this.controller = new Controller(this.game);
 
     // Enable physics.
-    this.game.physics.startSystem(Phaser.Physics.ARCADE);
+    this.game.physics.startSystem(Phaser.Physics.P2JS);
 
     // Example of the main character.
     this.character = this.game.add.sprite(0, 64 * 4, 'characters', 325);
@@ -77,9 +77,10 @@ export default class Main extends Phaser.State {
     } else if (this.controller.isUp) {
       this.character.y += 8;
     }
-    this.monsters.forEach(monster =>
-      this.game.physics.arcade.moveToObject(monster, this.character, 200)
-    );
+    // this.monsters.forEach(monster =>
+    //   //this.game.physics.p2.(monster, this.character, 200)
+    //   //this.game.
+    // );
   }
 
   private monster(): Phaser.Sprite {
@@ -90,7 +91,7 @@ export default class Main extends Phaser.State {
       162
     );
     monster.scale = this.character.scale;
-    this.game.physics.enable(monster, Phaser.Physics.ARCADE);
+    this.game.physics.p2.enable(monster);
     return monster;
   }
 }
