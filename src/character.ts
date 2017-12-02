920d7344af55fa906f3cb4851fbafb10f4b46fe8
QuantumBits <classicrockjunkie@gmail.com>
2017-12-02 01:29:08 -0800

    Real updates to stuff

--- src/character.ts
+++ src/character.ts
@@ -3,22 +3,49 @@
  * Encapsulates all information about a character
  */ 
 class Character {
-
-    name: string;
-
-    speed: number;
     
-    x: number;
-    y: number;
+    private name: string;
     
-    constructor(name: string, x: number, y: number, speed: number) {
+    private speed: number;
+    
+    private x: number;
+    private y: number;
+    
+    private str: number;
+    private int: number;
+    private cha: number;
+
+    private isGuard: boolean;
+    
+    constructor(name: string, x: number, y: number, speed: number, str: number = 1, int: number = 1, cha: number = 1, isGuard: boolean = false) {
         
         this.name = name;
         this.setSpeed(speed);
         this.moveTo(x, y);
+        
+        this.setStr(str);
+        this.setInt(int);
+        this.setCha(cha);
+        this.isGuard = isGuard;
     }
     
-    setSpeed(speed: number) {
+    public getName(): string {
+        return this.name;
+    }
+
+    public getSpeed(): number {
+        return this.speed;
+    }
+
+    public getX(): number {
+        return this.x;
+    }
+
+    public getY(): number {
+        return this.y;
+    }
+
+    public setSpeed(speed: number) {
         if (speed > 0 ) {
         this.speed = speed;
         }
@@ -27,51 +54,58 @@ class Character {
         }
     }
 
-    moveBy(dx: number, dy: number): void {
+    public moveBy(dx: number, dy: number): void {
         this.x = this.x + dx;
         this.y = this.y + dy;
     }
     
-    moveTo(x: number, y: number): void {
+    public moveTo(x: number, y: number): void {
         if (x > 0) { this.x = x; } else { throw new RangeError("x value must be > 0"); }
         if (y > 0) { this.y = y; } else { throw new RangeError("y value must be > 0"); }
     }
     
-}
-
-
-class Guard extends Character {
     
-    str: number;
-    int: number;
-    cha: number;
-    
-    constructor(name: string, x: number, y: number, speed: number,
-                str: number, int: number, cha: number) {
-        super(name, x, y, speed);
-        this.setStr(str);
-        this.setInt(int);
-        this.setCha(cha);
+    public getSTR(): number {
+        return this.str;
+    }
+    public getINT(): number {
+        return this.int;
+    }
+    public getCHA(): number {
+        return this.cha;
     }
     
+    public getIsGuard(): boolean {
+        return this.isGuard;
+    }
     
-    setStr(str: number): void {
+    public handleCrisis(crisis: Crisis): boolean {
+        // Logic for choosing a crisis option here
+        if (this.isGuard) {
+            return crisis.getOptions()[Math.floor(Math.random()*crisis.getOptions().length)].choose(this);
+        }
+        else {
+            return false;
+        }
+    }
+
+    private setStr(str: number): void {
         if (str > 0) { this.str = str; } else { throw new RangeError("str value must be > 0"); }
     }
     
-    setInt(int: number): void {
+    private setInt(int: number): void {
         if (int > 0) { this.int = int; } else { throw new RangeError("int value must be > 0"); }
     }
     
-    setCha(cha: number): void {
+    private setCha(cha: number): void {
         if (cha > 0) { this.cha = cha; } else { throw new RangeError("cha value must be > 0"); }
     }
+    
 }
-
 
 function generateName(): string {
     
-    let names: Array<string> = ["Bob", "Joe", "Bill", "Steve", "Eric", "Donald", "Jared", "Robert", "Paul"];
+    const names: string[] = ["Bob", "Joe", "Bill", "Steve", "Eric", "Donald", "Jared", "Robert", "Paul"];
     
     return names[Math.floor(Math.random()*names.length)];
     
