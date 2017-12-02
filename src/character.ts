d5800d9d82f681899e059edec878b546636116af
QuantumBits <classicrockjunkie@gmail.com>
2017-12-02 01:43:09 -0800

    Prettification!

--- src/character.ts
+++ src/character.ts
@@ -1,112 +1,146 @@
 /**
  * Character class
  * Encapsulates all information about a character
- */ 
+ */
+
 class Character {
-    
-    private name: string;
-    
-    private speed: number;
-    
-    private x: number;
-    private y: number;
-    
-    private str: number;
-    private int: number;
-    private cha: number;
+  private name: string;
 
-    private isGuard: boolean;
-    
-    constructor(name: string, x: number, y: number, speed: number, str: number = 1, int: number = 1, cha: number = 1, isGuard: boolean = false) {
-        
-        this.name = name;
-        this.setSpeed(speed);
-        this.moveTo(x, y);
-        
-        this.setStr(str);
-        this.setInt(int);
-        this.setCha(cha);
-        this.isGuard = isGuard;
-    }
-    
-    public getName(): string {
-        return this.name;
-    }
+  private speed: number;
 
-    public getSpeed(): number {
-        return this.speed;
-    }
+  private x: number;
+  private y: number;
 
-    public getX(): number {
-        return this.x;
-    }
+  private str: number;
+  private int: number;
+  private cha: number;
 
-    public getY(): number {
-        return this.y;
-    }
+  private isGuard: boolean;
 
-    public setSpeed(speed: number) {
-        if (speed > 0 ) {
-        this.speed = speed;
-        }
-        else {
-            throw new RangeError("Speed must be > 0");
-        }
-    }
+  constructor(
+    name: string,
+    x: number,
+    y: number,
+    speed: number,
+    str: number = 1,
+    int: number = 1,
+    cha: number = 1,
+    isGuard: boolean = false
+  ) {
+    this.name = name;
+    this.setSpeed(speed);
+    this.moveTo(x, y);
 
-    public moveBy(dx: number, dy: number): void {
-        this.x = this.x + dx;
-        this.y = this.y + dy;
-    }
-    
-    public moveTo(x: number, y: number): void {
-        if (x > 0) { this.x = x; } else { throw new RangeError("x value must be > 0"); }
-        if (y > 0) { this.y = y; } else { throw new RangeError("y value must be > 0"); }
-    }
-    
-    
-    public getSTR(): number {
-        return this.str;
-    }
-    public getINT(): number {
-        return this.int;
-    }
-    public getCHA(): number {
-        return this.cha;
-    }
-    
-    public getIsGuard(): boolean {
-        return this.isGuard;
-    }
-    
-    public handleCrisis(crisis: Crisis): boolean {
-        // Logic for choosing a crisis option here
-        if (this.isGuard) {
-            return crisis.getOptions()[Math.floor(Math.random()*crisis.getOptions().length)].choose(this);
-        }
-        else {
-            return false;
-        }
-    }
+    this.setStr(str);
+    this.setInt(int);
+    this.setCha(cha);
+    this.isGuard = isGuard;
+  }
 
-    private setStr(str: number): void {
-        if (str > 0) { this.str = str; } else { throw new RangeError("str value must be > 0"); }
+  public getName(): string {
+    return this.name;
+  }
+
+  public getSpeed(): number {
+    return this.speed;
+  }
+
+  public getX(): number {
+    return this.x;
+  }
+
+  public getY(): number {
+    return this.y;
+  }
+
+  public setSpeed(speed: number) {
+    if (speed > 0) {
+      this.speed = speed;
+    } else {
+      throw new RangeError('Speed must be > 0');
     }
-    
-    private setInt(int: number): void {
-        if (int > 0) { this.int = int; } else { throw new RangeError("int value must be > 0"); }
+  }
+
+  public moveBy(dx: number, dy: number): void {
+    this.x = this.x + dx;
+    this.y = this.y + dy;
+  }
+
+  public moveTo(x: number, y: number): void {
+    if (x > 0) {
+      this.x = x;
+    } else {
+      throw new RangeError('x value must be > 0');
     }
-    
-    private setCha(cha: number): void {
-        if (cha > 0) { this.cha = cha; } else { throw new RangeError("cha value must be > 0"); }
+    if (y > 0) {
+      this.y = y;
+    } else {
+      throw new RangeError('y value must be > 0');
     }
-    
+  }
+
+  public getSTR(): number {
+    return this.str;
+  }
+  public getINT(): number {
+    return this.int;
+  }
+  public getCHA(): number {
+    return this.cha;
+  }
+
+  public getIsGuard(): boolean {
+    return this.isGuard;
+  }
+
+  public handleCrisis(crisis: Crisis): boolean {
+    // Logic for choosing a crisis option here
+    if (this.isGuard) {
+      return crisis
+        .getOptions()
+        [Math.floor(Math.random() * crisis.getOptions().length)].choose(this);
+    } else {
+      return false;
+    }
+  }
+
+  private setStr(str: number): void {
+    if (str > 0) {
+      this.str = str;
+    } else {
+      throw new RangeError('str value must be > 0');
+    }
+  }
+
+  private setInt(int: number): void {
+    if (int > 0) {
+      this.int = int;
+    } else {
+      throw new RangeError('int value must be > 0');
+    }
+  }
+
+  private setCha(cha: number): void {
+    if (cha > 0) {
+      this.cha = cha;
+    } else {
+      throw new RangeError('cha value must be > 0');
+    }
+  }
 }
 
 function generateName(): string {
-    
-    const names: string[] = ["Bob", "Joe", "Bill", "Steve", "Eric", "Donald", "Jared", "Robert", "Paul"];
-    
-    return names[Math.floor(Math.random()*names.length)];
-    
+  const names: string[] = [
+    'Bob',
+    'Joe',
+    'Bill',
+    'Steve',
+    'Eric',
+    'Donald',
+    'Jared',
+    'Robert',
+    'Paul',
+  ];
+
+  return names[Math.floor(Math.random() * names.length)];
 }
--- src/crisis/crisis.ts
+++ src/crisis/crisis.ts
@@ -1,123 +1,124 @@
 /**
  * class Event
  * Encapsulates information concerning a single event.
- * 
+ *
  * description: string
  *  A description of the event that is unfolding.
- * 
+ *
  * options: Array<string>
  *  A list of options a Character can choose from to resolve the Event
- * 
+ *
  * timeLimit: number
  *  The maximum time limit that the Event has before it must be resolved
- * 
+ *
  * baseValue: number
  *  Number that determines how much this event impacts the player's score
- * 
+ *
  * townsfolk: Character
  *  Automatically generated Character designed to lead Player / Guard to GameEvent
- * 
+ *
  */
- class Crisis {
+class Crisis {
+  private description: string;
+  private options: CrisisOption[];
+  private timeLimit: number;
 
-    private description: string;
-    private options: CrisisOption[];
-    private timeLimit: number;
-    
-    private baseValue: number;
-    
-    private townsfolk: Character;
-    
-    private resolver: Character;
-    private choice: CrisisOption;
-    
-    private resolved: boolean;
-    private timeStart: number;
-    private timeResolved: number;
+  private baseValue: number;
 
-    constructor(description: string, timeLimit: number, baseValue: number, townsfolk: Character, timeStart: number) {
-        this.description = description;
+  private townsfolk: Character;
 
-        this.setTimeLimit(timeLimit);
+  private resolver: Character;
+  private choice: CrisisOption;
 
-        this.baseValue = baseValue;
-        this.townsfolk = townsfolk;
+  private resolved: boolean;
+  private timeStart: number;
+  private timeResolved: number;
 
-        this.resolved = false;
-        this.timeStart = timeStart;
-    }
+  constructor(
+    description: string,
+    timeLimit: number,
+    baseValue: number,
+    townsfolk: Character,
+    timeStart: number
+  ) {
+    this.description = description;
 
-    public getDescription(): string {
-        return this.description;
-    }
+    this.setTimeLimit(timeLimit);
 
-    public getValue(): number {
-        // This needs to get a lot more complicated
-        return this.baseValue;
-    }
+    this.baseValue = baseValue;
+    this.townsfolk = townsfolk;
 
-    public getTownsfolk(): Character {
-        return this.townsfolk;
-    }
+    this.resolved = false;
+    this.timeStart = timeStart;
+  }
 
-    public addOption(description: string, value: number): void {
-        this.options.push(new CrisisOption(this, description, value));
+  public getDescription(): string {
+    return this.description;
+  }
+
+  public getValue(): number {
+    // This needs to get a lot more complicated
+    return this.baseValue;
+  }
+
+  public getTownsfolk(): Character {
+    return this.townsfolk;
+  }
+
+  public addOption(description: string, value: number): void {
+    this.options.push(new CrisisOption(this, description, value));
+  }
+
+  /**
+   * If a guard attempts to claim this crisis and its resolver is not null,
+   * then return false.
+   * Otherwise, set the resolver to the guard and its choice
+   * @param guard
+   * @param choice
+   */
+  public claim(guard: Character, choice: CrisisOption): boolean {
+    if (this.resolver == null) {
+      this.resolver = guard;
+      this.choice = choice;
+      return true;
     }
-    
-    /**
-     * If a guard attempts to claim this crisis and its resolver is not null,
-     * then return false.
-     * Otherwise, set the resolver to the guard and its choice
-     * @param guard 
-     * @param choice 
-     */
-    public claim(guard: Character, choice: CrisisOption): boolean {
-        if (this.resolver == null) {
-            this.resolver = guard;
-            this.choice = choice;
-            return true;
-        }
-        return false;
+    return false;
+  }
+
+  public resolve(time: number) {
+    this.timeResolved = time;
+    this.resolved = true;
+  }
+
+  public getGuard(): Character {
+    return this.resolver;
+  }
+
+  public isResolved(): boolean {
+    return this.resolved;
+  }
+
+  public getChoice(): CrisisOption {
+    return this.choice;
+  }
+
+  public getTimeToResolve(): number {
+    return this.timeResolved - this.timeStart;
+  }
+
+  public getResolvedInTime(): boolean {
+    return this.timeResolved - this.timeStart <= this.timeLimit;
+  }
+
+  public getOptions(): CrisisOption[] {
+    return this.options;
+  }
+
+  private setTimeLimit(timeLimit: number): void {
+    if (timeLimit > 0) {
+      this.timeLimit = timeLimit;
+    } else {
+      throw new RangeError('Invalid timeLimit. Must be > 0');
     }
-    
-    public resolve(time: number) {
-        this.timeResolved = time;
-        this.resolved = true;
-    }
-    
-    public getGuard(): Character {
-        return this.resolver;
-    }
-    
-    public isResolved(): boolean {
-        return this.resolved;
-    }
-    
-    public getChoice(): CrisisOption {
-        return this.choice;
-    }
-    
-    public getTimeToResolve(): number {
-        return this.timeResolved - this.timeStart;
-    }
-    
-    public getResolvedInTime(): boolean {
-        return this.timeResolved - this.timeStart <= this.timeLimit;
-    }
-    
-    public getOptions(): CrisisOption[] {
-        return this.options;
-    }
-    
-    private setTimeLimit(timeLimit: number): void {
-        if (timeLimit > 0) {
-            this.timeLimit = timeLimit;
-        } 
-        else {
-            throw new RangeError("Invalid timeLimit. Must be > 0");
-        }
-    }
-    
+  }
 }
-
-
