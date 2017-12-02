299b76b6a1c5714926a11b2ed8efc7eece0ee1c2
Matan Lurey <matan@lurey.org>
2017-12-02 14:27:18 -0800

    Add options box.

--- src/ui/message.ts
+++ src/ui/message.ts
@@ -1,13 +1,23 @@
 import * as Phaser from 'phaser-ce';
+import Controller from '../input/controller';
 
 export default class MessagePanel extends Phaser.Plugin {
+  private controller: Controller;
+
   private text: Phaser.Text;
+
+  private oSprite: Phaser.Sprite;
+  private option1: Phaser.Text;
+  private option2: Phaser.Text;
+  private callback: (option: number) => void;
 
   constructor(game: Phaser.Game, manager: Phaser.PluginManager) {
     super(game, manager);
   }
 
-  public init(group: Phaser.Group): void {
+  public init(group: Phaser.Group, controller: Controller): void {
+    this.controller = controller;
+
     const game = this.game;
     const bitmap = game.add.bitmapData(game.width, game.height);
 
@@ -22,20 +32,84 @@ export default class MessagePanel extends Phaser.Plugin {
     group.add(sprite);
 
     const text = game.add.text(0, 0, '...', {
-      boundsAlignH: 'center',
+      boundsAlignH: 'left',
       boundsAlignV: 'middle',
       fill: '#FFF',
       font: 'bold 24px Arial',
     });
     text.setShadow(3, 3, 'rgba(0, 0, 0, 0.5)', 2);
-    text.setTextBounds(0, game.height - 100, game.width, 100);
+    text.setTextBounds(50, game.height - 100, game.width, 100);
     text.fixedToCamera = true;
     group.add(text);
 
     this.text = text;
+
+    const options = game.add.bitmapData(game.width, game.height);
+
+    options.ctx.beginPath();
+    options.ctx.rect(600, game.height - 90, 190, 80);
+    options.ctx.fillStyle = '#787C8B';
+    options.ctx.fill();
+    options.ctx.strokeStyle = '#565869';
+    options.ctx.strokeRect(600, game.height - 90, 190, 80);
+    this.oSprite = game.add.sprite(0, 0, options);
+    this.oSprite.fixedToCamera = true;
+    group.add(this.oSprite);
+
+    this.option1 = game.add.text(0, 0, '[ 1 ]   Say Hello', {
+      fill: '#FFF',
+      font: '13px Arial',
+    });
+    this.option2 = game.add.text(0, 0, '[ 2 ]   Say Goodbye', {
+      fill: '#FFF',
+      font: '13px Arial',
+    });
+    this.option1.setTextBounds(
+      game.width - 180,
+      game.height - 80,
+      game.width,
+      40
+    );
+    this.option2.setTextBounds(
+      game.width - 180,
+      game.height - 40,
+      game.width,
+      40
+    );
+    group.add(this.option1);
+    group.add(this.option2);
+
+    this.oSprite.visible = false;
+    this.option1.visible = false;
+    this.option2.visible = false;
+  }
+
+  public askUser(
+    option1: string,
+    option2: string,
+    callback: (option: number) => void
+  ): void {
+    this.oSprite.visible = true;
+    this.callback = callback;
+    this.option1.text = `[ 1 ] ${option1}`;
+    this.option1.visible = true;
+    this.option2.text = `[ 2 ] ${option2}`;
+    this.option2.visible = true;
   }
 
   public setText(message: string): void {
     this.text.text = message;
+  }
+
+  public update(): void {
+    if (this.oSprite.visible) {
+      if (this.controller.is1) {
+        this.callback(1);
+        this.oSprite.visible = this.option1.visible = this.option2.visible = false;
+      } else if (this.controller.is2) {
+        this.callback(2);
+        this.oSprite.visible = this.option1.visible = this.option2.visible = false;
+      }
+    }
   }
 }
--- src/ui/states/main.ts
+++ src/ui/states/main.ts
@@ -4,6 +4,7 @@ import { forIn, last } from 'lodash';
 import Controller from '../../input/controller';
 import MessagePanel from '../message';
 
+import * as common from '../../common';
 import { game } from '../../index';
 
 /**
@@ -33,8 +34,17 @@ export default class Main extends Phaser.State {
 
     // Messages.
     this.alwaysOnTop = this.game.add.group();
-    this.messages = this.game.plugins.add(MessagePanel, this.alwaysOnTop);
-    this.messages.setText('🔥🔥 CRISIS! 🔥🔥');
+    this.messages = this.game.plugins.add(
+      MessagePanel,
+      this.alwaysOnTop,
+      this.controller
+    );
+    this.messages.setText('Welcome to Guard Captain');
+    this.messages.askUser('Sushi', 'Tacos', option =>
+      common.debug.log(
+        `Selected: ${option === 1 ? 'Great Choice' : 'Eh, not bad'}`
+      )
+    );
   }
 
   public update(): void {
