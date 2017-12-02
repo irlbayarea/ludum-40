8c9e7b926d85270070ada93a1bec1a029c235143
QuantumBits <classicrockjunkie@gmail.com>
2017-12-02 14:36:50 -0800

    Fixing Character / CrisisOption constructor bounds checking

--- src/character/character.ts
+++ src/character/character.ts
@@ -56,7 +56,7 @@ export default class Character {
     this.name = name;
     this.sprite = sprite;
 
-    if (Character.maxSpeed > speed && speed > Character.minSpeed) {
+    if (Character.maxSpeed >= speed && speed >= Character.minSpeed) {
       this.speed = speed;
     } else {
       throw new RangeError(
@@ -67,7 +67,7 @@ export default class Character {
           ']'
       );
     }
-    if (Character.maxStrength > strength && strength > Character.minStrength) {
+    if (Character.maxStrength >= strength && strength >= Character.minStrength) {
       this.strength = strength;
     } else {
       throw new RangeError(
@@ -79,8 +79,8 @@ export default class Character {
       );
     }
     if (
-      Character.maxIntelligence > intelligence &&
-      Character.minIntelligence > intelligence
+      Character.maxIntelligence >= intelligence &&
+      Character.minIntelligence >= intelligence
     ) {
       this.intelligence = intelligence;
     } else {
@@ -92,7 +92,7 @@ export default class Character {
           ']'
       );
     }
-    if (Character.maxCharisma > charisma && charisma > Character.minCharisma) {
+    if (Character.maxCharisma >= charisma && charisma >= Character.minCharisma) {
       this.charisma = charisma;
     } else {
       throw new RangeError(
@@ -103,7 +103,7 @@ export default class Character {
           ']'
       );
     }
-    if (Character.maxGoodness > goodness && goodness > Character.minGoodness) {
+    if (Character.maxGoodness >= goodness && goodness >= Character.minGoodness) {
       this.goodness = goodness;
     } else {
       throw new RangeError(
@@ -115,8 +115,8 @@ export default class Character {
       );
     }
     if (
-      Character.maxRandomness > randomness &&
-      randomness > Character.minRandomness
+      Character.maxRandomness >= randomness &&
+      randomness >= Character.minRandomness
     ) {
       this.randomness = randomness;
     } else {
