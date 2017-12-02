ac32bc1225bd826365e241335cab39fe6d6aa837
QuantumBits <classicrockjunkie@gmail.com>
2017-12-02 01:26:48 -0800

    Updates to crisis / character and adding option.ts. Now in crisis subfolder

---
+++ src/crisis/option.ts
@@ -0,0 +1,32 @@
+/**
+ * CrisisOption
+ * Encapsulates a game event option.
+ */
+class CrisisOption {
+    
+        private parent: Crisis
+        private description: string;
+        private value: number;
+    
+        constructor(parent: Crisis, description: string, value: number) {
+            this.parent = parent;
+            this.description = description;
+            this.value = value;
+        }
+    
+        public choose(guard: Character): boolean {
+            return this.parent.claim(guard, this);
+        }
+    
+        public getParent(): Crisis {
+            return this.parent;
+        }
+    
+        public getDescription(): string {
+            return this.description;
+        }
+    
+        public getValue(): number {
+            return this.value;
+        }
+    }
