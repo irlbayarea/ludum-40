00de531fd3943e6d929329fb9320a2843ce76e5d
Matan Lurey <matanlurey@users.noreply.github.com>
2017-12-01 23:10:30 -0800

    Add basic cursor movement. (#10)

--- src/ui/states/main.ts
+++ src/ui/states/main.ts
@@ -4,6 +4,8 @@ import * as Phaser from 'phaser-ce';
  * Main state (i.e. in the game).
  */
 export default class Main extends Phaser.State {
+  private cursors: Phaser.CursorKeys;
+
   public create(): void {
     const map = this.game.add.tilemap('Tilemap');
     map.addTilesetImage('tiles', 'tiles');
@@ -11,9 +13,23 @@ export default class Main extends Phaser.State {
     const layer = map.createLayer('terrain');
     layer.resizeWorld();
     layer.wrap = true;
+    this.cursors = this.game.input.keyboard.createCursorKeys();
   }
 
   public init(): void {
     this.stage.backgroundColor = '#EDEEC9';
+  }
+
+  public update(): void {
+    if (this.cursors.left.isDown) {
+      this.game.camera.x -= 8;
+    } else if (this.cursors.right.isDown) {
+      this.game.camera.x += 8;
+    }
+    if (this.cursors.up.isDown) {
+      this.game.camera.y -= 8;
+    } else if (this.cursors.down.isDown) {
+      this.game.camera.y += 8;
+    }
   }
 }
