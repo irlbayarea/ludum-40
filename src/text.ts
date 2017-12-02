af713b10c30a1dac061837e195f9f92c8d7c4c81
kharland <kendaljharland@gmail.com>
2017-12-02 02:02:10 -0800

    Draw speech on character

--- src/text.ts
+++ src/text.ts
@@ -1,5 +1,13 @@
 import { Game } from 'phaser-ce';
+import { game } from './index';
 
-export function renderText(game: Game, text: string, x: number, y: number) {
+// Writes the given speech as if the given character said it.
+// FIXME: Use character class when present.
+export function speak(character: any, speech: string) {
+    renderText(game, speech, character.getX(), character.getY() - 10)
+}
+
+function renderText(game: Game, text: string, x: number, y: number) {
   game.add.text(x, y, text, { font: '15px Arial', fill: 'purple' });
 }
+
