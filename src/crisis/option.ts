ffd602d3ef9cc25c6042d6688a4417391b0babf2
QuantumBits <classicrockjunkie@gmail.com>
2017-12-02 04:00:07 -0800

    Crisis of character (#25)
    * Add Kenney Game Assets spritesheet.
    
    * npm install'ed all the things
    
    * Render a bush, load with a boot.
    
    * Format.
    
    * Adding character.ts file to encapsulate Character information in a class
    
    * Added setters (don't need getters)
    
    * Adding basic GameEvent changes (still under construction)
    
    * Changed events.ts to crisis.ts, added more logical crisis management, added Guard type to character.ts
    
    * Updates to crisis / character and adding option.ts. Now in crisis subfolder
    
    * Real updates to stuff
    
    * Prettification!
    
    * Add message panel.
    
    * Add message panel.
    
    * Fix keyboard.
    
    * Added guard crisis option choice logic. Probably awful but who cares. Added 'Rando' and 'Good' properties to Character / CrisisOption.
    
    * Line widths now all < 80 >:(
    
    * ajfkdlsahhh
    
    * fdsafdsafdsafa
    
    * dfadfasfdafdsafdsafdsafdsafdsaf
    
    * fadafdasfs
    
    * hgdfsd

--- src/crisis/option.ts
+++ src/crisis/option.ts
@@ -1,16 +1,63 @@
+import Character from '../character';
+import Crisis from './crisis';
 /**
  * CrisisOption
  * Encapsulates a game event option.
  */
-class CrisisOption {
+export default class CrisisOption {
   private parent: Crisis;
   private description: string;
   private value: number;
 
-  constructor(parent: Crisis, description: string, value: number) {
+  private strVal: number;
+  private intVal: number;
+  private chaVal: number;
+  private goodVal: number;
+
+  constructor(
+    parent: Crisis,
+    description: string,
+    value: number,
+    strVal: number = 0,
+    intVal: number = 0,
+    chaVal: number = 0,
+    goodVal: number = 1
+  ) {
     this.parent = parent;
     this.description = description;
     this.value = value;
+    if (strVal >= 0) {
+      this.strVal = strVal;
+    } else {
+      throw new RangeError('strVal must be >= 0');
+    }
+    if (intVal >= 0) {
+      this.intVal = intVal;
+    } else {
+      throw new RangeError('intVal must be >= 0');
+    }
+    if (chaVal >= 0) {
+      this.chaVal = chaVal;
+    } else {
+      throw new RangeError('chaVal must be >= 0');
+    }
+    this.goodVal = goodVal;
+  }
+
+  public getSTR(): number {
+    return this.strVal;
+  }
+
+  public getINT(): number {
+    return this.intVal;
+  }
+
+  public getCHA(): number {
+    return this.chaVal;
+  }
+
+  public getGOOD(): number {
+    return this.goodVal;
   }
 
   public choose(guard: Character): boolean {
