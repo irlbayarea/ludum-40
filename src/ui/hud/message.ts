765e9768b1f9d9694eb197361ceb3172fcf8f487
QuantumBits <classicrockjunkie@gmail.com>
2017-12-03 05:05:09 -0800

    A whole new woooooorld!

--- src/ui/hud/message.ts
+++ src/ui/hud/message.ts
@@ -1,57 +1,81 @@
+// import { Message } from '_debugger';
 import * as Phaser from 'phaser-ce';
-// import { debug } from '../common';
+import { globals } from '../../common';
 
 export default class MessagePanel extends Phaser.Plugin {
+  public static readonly gr = 0.5 * (1 + Math.sqrt(5));
+  public static readonly mainPanel = new Phaser.Rectangle(
+    0,
+    globals.dimensions.height * (1 - 1 / MessagePanel.gr ** 3),
+    globals.dimensions.width,
+    globals.dimensions.height * (1 / MessagePanel.gr ** 3)
+  );
+
+  public static readonly textPadX: number = globals.dimensions.width /
+    MessagePanel.gr ** 8;
+  public static readonly textPadY: number = globals.dimensions.height /
+    MessagePanel.gr ** 8;
+  public static readonly numOptions: number = 4;
+
+  public static readonly mainTextPanel = new Phaser.Rectangle(
+    MessagePanel.mainPanel.x + MessagePanel.textPadX,
+    MessagePanel.mainPanel.y + MessagePanel.textPadY,
+    MessagePanel.mainPanel.width / (MessagePanel.gr ** MessagePanel.gr) -
+      2 * MessagePanel.textPadX,
+    MessagePanel.mainPanel.height - 2 * MessagePanel.textPadY
+  );
+
+  public static readonly choicePanel = new Phaser.Rectangle(
+    MessagePanel.mainTextPanel.x +
+      MessagePanel.mainTextPanel.width +
+      2 * MessagePanel.textPadX,
+    MessagePanel.mainTextPanel.y,
+    MessagePanel.mainPanel.width -
+      MessagePanel.mainTextPanel.width -
+      4 * MessagePanel.textPadX,
+    MessagePanel.mainTextPanel.height
+  );
+
+  public static readonly messageClearCountdownTIme: number = 200;
+  
+  private messageClearCountdown: number = 0;
+
   private text: Phaser.Text;
 
-  // private hudYSpeed: number = 0;
-
-  private panelSprite: Phaser.Sprite;
-  private oSprite: Phaser.Sprite;
+  private mainPanel: Phaser.Sprite;
+  private choicePanel: Phaser.Sprite;
   private optionList: Phaser.Text[] = [];
 
+
+  /**
+   * MessagePanel constructor
+   */
   constructor(game: Phaser.Game, manager: Phaser.PluginManager) {
     super(game, manager);
   }
 
   public init(group: Phaser.Group): void {
     const game = this.game;
-    const bitmap = game.add.bitmapData(game.width, game.height);
-
-    const gr = 0.5 * (1 + Math.sqrt(5));
-    const mainPanel = new Phaser.Rectangle(
-      0,
-      game.height * (1 - 1 / gr ** 3),
-      game.width,
-      game.height * (1 / gr ** 3)
+    const bitmap = game.add.bitmapData(
+      globals.dimensions.width,
+      globals.dimensions.height
     );
 
     bitmap.ctx.beginPath();
-    bitmap.ctx.globalAlpha = 1 / gr;
+    bitmap.ctx.globalAlpha = 1 / MessagePanel.gr;
     bitmap.ctx.rect(
-      mainPanel.x,
-      mainPanel.y,
-      mainPanel.width,
-      mainPanel.height
+      MessagePanel.mainPanel.x,
+      MessagePanel.mainPanel.y,
+      MessagePanel.mainPanel.width,
+      MessagePanel.mainPanel.height
     );
     bitmap.ctx.fillStyle = '#333333';
     bitmap.ctx.fill();
 
-    this.panelSprite = game.add.sprite(0, 0, bitmap);
-    this.panelSprite.fixedToCamera = true;
-    this.panelSprite.physicsEnabled = true;
-    group.add(this.panelSprite);
-
-    const textPadX = mainPanel.width / gr ** 8;
-    const textPadY = mainPanel.height / gr ** 4;
-    const numLines = 4;
-
-    const mainTextPanel = new Phaser.Rectangle(
-      mainPanel.x + textPadX,
-      mainPanel.y + textPadY,
-      mainPanel.width / gr ** gr - 2 * textPadX,
-      mainPanel.height - 2 * textPadY
-    );
+    this.mainPanel = game.add.sprite(0, 0, bitmap);
+    this.mainPanel.fixedToCamera = true;
+    this.mainPanel.physicsEnabled = true;
+    group.add(this.mainPanel);
 
     const text = game.add.text(0, 0, '...', {
       boundsAlignH: 'center',
@@ -59,68 +83,83 @@ export default class MessagePanel extends Phaser.Plugin {
       align: 'center',
       fill: '#FFF',
       wordWrap: true,
-      wordWrapWidth: mainTextPanel.width,
-      font: 'bold ' + mainTextPanel.height / numLines + 'px Consolas',
+      wordWrapWidth: MessagePanel.mainTextPanel.width,
+      font:
+        'bold ' +
+        MessagePanel.mainTextPanel.height / MessagePanel.numOptions +
+        'px Consolas',
     });
+    console.log(MessagePanel.mainTextPanel.height / MessagePanel.numOptions);
+    console.log(MessagePanel.mainTextPanel.height);
+    console.log(MessagePanel.numOptions);
     text.setShadow(3, 3, 'rgba(0, 0, 0, 0.5)', 2);
     text.setTextBounds(
-      mainTextPanel.x,
-      mainTextPanel.y,
-      mainTextPanel.width,
-      mainTextPanel.height
+      MessagePanel.mainTextPanel.x,
+      MessagePanel.mainTextPanel.y,
+      MessagePanel.mainPanel.width,
+      MessagePanel.mainTextPanel.height
     );
     text.fixedToCamera = true;
     group.add(text);
 
     this.text = text;
 
-    const options = game.add.bitmapData(game.width, game.height);
-
-    const choicePanel = new Phaser.Rectangle(
-      mainTextPanel.x + mainTextPanel.width + 2 * textPadX,
-      mainTextPanel.y,
-      mainPanel.width - mainTextPanel.width - 4 * textPadX,
-      mainTextPanel.height
+    const options = game.add.bitmapData(
+      globals.dimensions.width,
+      globals.dimensions.height
     );
 
     options.ctx.beginPath();
     options.ctx.rect(
-      choicePanel.x,
-      choicePanel.y,
-      choicePanel.width,
-      choicePanel.height
+      MessagePanel.choicePanel.x,
+      MessagePanel.choicePanel.y,
+      MessagePanel.choicePanel.width,
+      MessagePanel.choicePanel.height
     );
     options.ctx.fillStyle = '#787C8B';
     options.ctx.fill();
     options.ctx.strokeStyle = '#565869';
     options.ctx.strokeRect(
-      choicePanel.x,
-      choicePanel.y,
-      choicePanel.width,
-      choicePanel.height
+      MessagePanel.choicePanel.x,
+      MessagePanel.choicePanel.y,
+      MessagePanel.choicePanel.width,
+      MessagePanel.choicePanel.height
     );
-    this.oSprite = game.add.sprite(0, 0, options);
-    this.oSprite.fixedToCamera = true;
-    this.oSprite.visible = true;
-    group.add(this.oSprite);
+    this.choicePanel = game.add.sprite(0, 0, options);
+    this.choicePanel.fixedToCamera = true;
+    this.choicePanel.visible = true;
+    group.add(this.choicePanel);
 
-    for (let i = 0; i < 4; i++) {
+    for (let i = 0; i < MessagePanel.numOptions; i++) {
       const choicePanelOption: Phaser.Rectangle = new Phaser.Rectangle(
-        choicePanel.x +
-          textPadX +
-          choicePanel.width / 2 * (i === 0 || i === 2 ? 0 : 1),
-        choicePanel.y +
-          textPadY * (2 / 3) +
-          choicePanel.height / 2 * (i === 0 || i === 1 ? 0 : 1),
-        choicePanel.width / 2,
-        choicePanel.height / 2
+        MessagePanel.choicePanel.x +
+          MessagePanel.textPadX +
+          MessagePanel.choicePanel.width /
+            2 *
+            (i % MessagePanel.numOptions === 0 ||
+            i % MessagePanel.numOptions === 2
+              ? 0
+              : 1),
+        MessagePanel.choicePanel.y +
+          MessagePanel.textPadY * (2 / 3) +
+          MessagePanel.choicePanel.height /
+            2 *
+            (i % MessagePanel.numOptions === 0 ||
+            i % MessagePanel.numOptions === 1
+              ? 0
+              : 1),
+        MessagePanel.choicePanel.width / 2,
+        MessagePanel.choicePanel.height / 2
       );
       this.optionList.push(
-        game.add.text(0, 0, '[ ]', {
+        game.add.text(0, 0, '', {
           fill: '#FFFFFF',
-          font: (choicePanel.height - 2 * textPadY) / 4 + 'px Consolas',
+          font:
+            (MessagePanel.choicePanel.height - 2 * MessagePanel.textPadY) / 4 +
+            'px Consolas',
           wordWrap: true,
-          wordWrapWidth: choicePanel.width / 2 - textPadX,
+          wordWrapWidth:
+            MessagePanel.choicePanel.width / 2 - MessagePanel.textPadX,
         })
       );
       this.optionList[i].setTextBounds(
@@ -136,10 +175,19 @@ export default class MessagePanel extends Phaser.Plugin {
   }
 
   public setOptions(optionListInput: string[]): void {
-    if (optionListInput.length > 4) {
-      throw new Error('> 4 questions not yet supported');
+    
+    if (optionListInput.length > MessagePanel.numOptions) {
+      throw new Error(
+        '>' + MessagePanel.numOptions + ' questions not yet supported'
+      );
     } else {
-      this.oSprite.visible = true;
+      this.choicePanel.visible = true;
+      this.mainPanel.visible = true;
+      this.text.setTextBounds(
+        MessagePanel.mainTextPanel.x, 
+        MessagePanel.mainTextPanel.y,
+        MessagePanel.mainTextPanel.width, 
+        MessagePanel.mainTextPanel.height);
       for (let i = 0; i < optionListInput.length; i++) {
         this.optionList[i].text = `[ ${i + 1} ] ${optionListInput[i]}`;
         this.optionList[i].visible = true;
@@ -148,13 +196,43 @@ export default class MessagePanel extends Phaser.Plugin {
   }
 
   public clearOptions(): void {
+
     for (const opt of this.optionList) {
-      opt.text = `[ - ]`;
+      opt.text = '';
       opt.visible = true;
     }
+    this.text.setTextBounds(
+      MessagePanel.mainTextPanel.x, 
+      MessagePanel.mainTextPanel.y,
+      MessagePanel.mainPanel.width, 
+      MessagePanel.mainTextPanel.height);
+    this.mainPanel.visible = false;
+    this.choicePanel.visible = false;
   }
 
   public setText(message: string): void {
+    this.text.visible = true;
     this.text.text = message;
   }
+
+  public clearText(): void {
+    this.text.text = '';
+    this.text.visible = false;
+  }
+
+  public startCountdown(): void {
+    this.messageClearCountdown = MessagePanel.messageClearCountdownTIme;
+  }
+
+  public countdown(): boolean {
+    if (this.messageClearCountdown > 0)
+    {
+      this.messageClearCountdown -= 1;
+      console.log(this.messageClearCountdown);
+      return this.messageClearCountdown <= 0
+    } else {
+      return false;
+    }
+  }
+
 }
