e7d4724f5b10abde32e833f8ae144bfd785a3522
Matan Lurey <matan@lurey.org>
2017-12-02 17:05:04 -0800

    Done.

--- src/ui/states/main.ts
+++ src/ui/states/main.ts
@@ -101,6 +101,9 @@ export default class Main extends Phaser.State {
   }
 
   private _createMap(): Phaser.Tilemap {
+    // Initialize the physics system (P2).
+    this.game.physics.startSystem(Phaser.Physics.P2JS);
+    
     if (common.experiment('use-generated-map')) {
       return this.createGeneratedMap();
     } else {
@@ -109,9 +112,6 @@ export default class Main extends Phaser.State {
   }
 
   private createDefaultMap(): Phaser.Tilemap {
-    // Initialize the physics system (P2).
-    this.game.physics.startSystem(Phaser.Physics.P2JS);
-
     // Create the map.
     const map = game.add.tilemap('Tilemap');
 
