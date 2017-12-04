9ebc9ff3025f5e3fee70be26322ae5da9b5fa783
QuantumBits <classicrockjunkie@gmail.com>
2017-12-03 22:24:39 -0800

    top hud menu done woo (#123)

--- src/ui/hud/text_widget.ts
+++ src/ui/hud/text_widget.ts
@@ -6,12 +6,22 @@ export default class TextWidget {
   constructor(
     private readonly game: Game,
     private readonly x: number,
-    private readonly y: number
+    private readonly y: number,
+    private readonly w: number,
+    private readonly h: number,
+    fontSize: number,
+    font: string
   ) {
     this.text = this.game.add.text(this.x, this.y, '', {
-      font: 'bold 15px Consolas',
-      fill: '#000',
+      font: 'bold ' + fontSize + 'px ' + font,
+      fill: '#FFFFFF',
+      boundsAlignH: 'center',
+      boundsAlignV: 'middle',
     });
+    this.text.setTextBounds(this.x / 4, this.y / 4, this.w, this.h);
+    this.text.setShadow(1, 1, 'rgba(0, 0, 0, 1)', 3, true, true);
+    this.text.align = 'center';
+    // this.text.anchor.set(0.5, 0.5);
     this.text.fixedToCamera = true;
   }
 
