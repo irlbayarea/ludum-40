d7083775943dfb1fe0766b0048fba7cf026e371b
Matan Lurey <matan@lurey.org>
2017-12-03 21:16:23 -0800

    Add hut explosions.

--- src/ui/sprites/blood.ts
+++ src/ui/sprites/blood.ts
@@ -7,13 +7,25 @@ export default class BloodFactory {
     this.game = game;
   }
 
-  public sprite(on: Phaser.Sprite): Phaser.Sprite {
+  public blood(on: Phaser.Sprite): Phaser.Sprite {
     const sprite = this.game.add.sprite(
       on.body.x - 48,
       on.body.y - 32,
       'blood'
     );
     sprite.scale = new Phaser.Point(0.25, 0.25);
+    sprite.animations.add('animate');
+    sprite.animations.play('animate', 30, false, true);
+    return sprite;
+  }
+
+  public boom(on: Phaser.Sprite): Phaser.Sprite {
+    const sprite = this.game.add.sprite(
+      on.x,
+      on.y,
+      'explode'
+    );
+    // sprite.scale = new Phaser.Point(0.25, 0.25);
     sprite.animations.add('animate');
     sprite.animations.play('animate', 30, false, true);
     return sprite;
